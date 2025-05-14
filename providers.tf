2066592fd2fbe7575369f78bc0e264711e017e70
Kevin Contreras Rodríguez <kvncont@gmail.com>
2025-05-14 05:16:41 +0000

    chore: improve the terraform template module

--- providers.tf
+++ providers.tf
@@ -1,16 +1,6 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-  assume_role {
-    role_arn     = var.assume_role_arn
-    session_name = var.assume_role_session_name
-  }
+  required_version = ">= 1.11"
+  
+  # Declare the required providers and their versions
+  required_providers {}
 }
